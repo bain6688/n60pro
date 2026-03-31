1d9ad402c4f78af52b5bdf74af79ea7299d2d8fe
bain6688 <932806255@qq.com>
2026-03-31 13:42:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part5-6.6.sh
+# File name: diy-part2.sh
 # Description: OpenWrt DIY script part 5 (After Update feeds)
 #
 # Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
@@ -18,22 +18,3 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-
-# 添加组播防火墙规则
-cat >> package/network/config/firewall/files/firewall.config <<EOF
-config rule
-        option name 'Allow-UDP-igmpproxy'
-        option src 'wan'
-        option dest 'lan'
-        option dest_ip '224.0.0.0/4'
-        option proto 'udp'
-        option target 'ACCEPT'        
-        option family 'ipv4'
-
-config rule
-        option name 'Allow-UDP-udpxy'
-        option src 'wan'
-        option dest_ip '224.0.0.0/4'
-        option proto 'udp'
-        option target 'ACCEPT'
-EOF
